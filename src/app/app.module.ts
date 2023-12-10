cb647c042380dbae1f38690ee467c4ad1d7004bc
syliaaal <akialsylia@gmail.com>
2023-12-10 16:37:12 +0100

    todolist2

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TodolistComponent } from './todolist/todolist.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodolistComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
